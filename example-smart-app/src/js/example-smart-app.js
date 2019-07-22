58c22c9a00f6b842cf527b3e20419ef2a4440563
Haiwen Gui <haiwen@google.com>
2019-07-22 11:09:42 -0400

    testing2

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,6 +27,7 @@
         var diagnostic = smart.patient.api.fetchAll({
           type: 'DiagnosticReport'
         });
+        console.log('diagnostic', diagnostic);
         console.log('.read and .fetchAll', pt, obv);
 
         $.when(pt, obv).fail(onError);
@@ -54,7 +55,7 @@
           var ldl = byCodes('2089-1');
           console.log('hdl', hdl);
           console.log('ldl', ldl);
-          console.log('trying other search', smart.patient.api.search({type: 'DiagnosticReport', query: queryObject}))
+          console.log('trying other search', smart.patient.api.search({type: 'DiagnosticReport'}));
           console.log('bloodpressure', byCodes('55284-4'));
 
           var p = defaultPatient();
